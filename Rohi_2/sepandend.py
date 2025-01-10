e7b9113579dcdfb25a3b7f7e4e19b060abf8fec4
Rohit <rohit.panwar90826@gmail.com>
2025-01-10 17:51:57 +0530

    variable & Datatype

---
+++ Rohi_2/sepandend.py
@@ -0,0 +1,24 @@
+from operator import truediv
+
+print("hello", "rohit", "panwar")
+#Seprator Command
+print("hello", "rohit", "panwar", sep="-")
+#end command
+print("hello", "rohit", "panwar", end=" ")
+#end and separator together
+print("hello", "rohit", "panwar", sep="-", end=" ")
+#max function
+print(max(1,2,3,4,5,6,7,8,9,10))
+
+#class and type
+age=23
+isMale=True
+pi=3.2
+print(type(age))
+print(type(isMale))
+print(type(pi))
+
+#complex number
+complex_number=2+3j
+print(complex_number.real)
+print(complex_number.imag)
